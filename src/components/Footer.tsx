1e0a28840784b89ea4dd6d7e7a4b296ae382853a
Baptiste May <baptiste_may@ens.univ-artois.fr>
2025-03-03 22:23:45 +0100

    Fix websocket security

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,5 +2,5 @@ export default function Footer({forMobile = false}: {
     forMobile?: boolean;
 }) {
     return <span
-        className={forMobile ? "flex lg:hidden justify-center" : "hidden lg:flex"}>© 2025 May Baptiste - v1.0.1</span>;
+        className={forMobile ? "flex lg:hidden justify-center" : "hidden lg:flex"}>© 2025 May Baptiste - v1.0.2</span>;
 }
